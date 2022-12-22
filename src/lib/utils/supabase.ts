b5fc339c5852b358b34316eecd114e90b12ceb3f
Mrlolpro <leonardoollero14@gmail.com>
2022-12-22 15:07:47 +0100

    fix: dotenv

--- src/lib/utils/supabase.ts
+++ src/lib/utils/supabase.ts
@@ -1,12 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
-import dotenv from 'dotenv';
-dotenv.config();
-const { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } = process.env;
-let supabase = null;
 
-if (PUBLIC_SUPABASE_URL && PUBLIC_SUPABASE_ANON_KEY) {
-	supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
-}
-
-export { supabase };
+export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
